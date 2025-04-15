a93bf8879e12b8eeea2dbd68be02b1f1d35e3b28
Daniil-local <dgilev75@gmail.com>
2025-04-15 13:36:16 +0300

    Revert "feat: add multiplication"
    This reverts commit 90e2f8bf8695f5c63fdf6ad85709e21ae0a4a41a.

--- src/features/Counter/Counter.tsx
+++ src/features/Counter/Counter.tsx
@@ -6,12 +6,10 @@ export const Counter = () => {
     const [counter, setCounter] = useState(0);
     const incCounter = () => setCounter(prev => prev += 1);
     const decCounter = () => setCounter(prev => prev -= 1);
-    const multCounter = () => setCounter(prev => prev *= 2);
     return (<div>
         <h3>Counter</h3>
         <p>{counter}</p>
         <button onClick={incCounter} name='+'>+</button>
         <button onClick={decCounter} name='-'>-</button>
-        <button onClick={multCounter} name='+'>*2</button>
     </div>)
 }
